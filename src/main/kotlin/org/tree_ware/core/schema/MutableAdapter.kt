d524c5042d765a3e13ab548823e6fa24586aca6c
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2020-02-08 23:37:35 -0800

    Adaptor to make a SchemaVisitor look like a MutableSchemaVisitor

---
+++ src/main/kotlin/org/tree_ware/core/schema/MutableAdapter.kt
@@ -0,0 +1,106 @@
+package org.tree_ware.core.schema
+
+// TODO(deepak-nulu): implement visitor-combinators to combine multiple visitors into a single traversal.
+
+/** Adapts a SchemaVisitor to make it look like a MutableSchemaVisitor.
+ * The SchemaVisitor cannot mutate the elements it visits (since the types of the elements passed to it are still
+ * the immutable types), but it can be used in mutable-visitor-combinators along with other MutableSchemaVisitor
+ * instances.
+ */
+class MutableAdapter(private val adaptee: SchemaVisitor) : MutableSchemaVisitor {
+    override fun mutableVisit(element: MutableElementSchema): Boolean {
+        return adaptee.visit(element)
+    }
+
+    override fun mutableVisit(namedElement: MutableNamedElementSchema): Boolean {
+        return adaptee.visit(namedElement)
+    }
+
+    override fun mutableVisit(schema: MutableSchema): Boolean {
+        return adaptee.visit(schema)
+    }
+
+    override fun mutableVisit(pkg: MutablePackageSchema): Boolean {
+        return adaptee.visit(pkg)
+    }
+
+    override fun mutableVisit(alias: MutableAliasSchema): Boolean {
+        return adaptee.visit(alias)
+    }
+
+    override fun mutableVisit(enumeration: MutableEnumerationSchema): Boolean {
+        return adaptee.visit(enumeration)
+    }
+
+    override fun mutableVisit(entity: MutableEntitySchema): Boolean {
+        return adaptee.visit(entity)
+    }
+
+    // Fields
+
+    override fun mutableVisit(field: MutableFieldSchema): Boolean {
+        return adaptee.visit(field)
+    }
+
+    override fun mutableVisit(primitiveField: MutablePrimitiveFieldSchema): Boolean {
+        return adaptee.visit(primitiveField)
+    }
+
+    override fun mutableVisit(aliasField: MutableAliasFieldSchema): Boolean {
+        return adaptee.visit(aliasField)
+    }
+
+    override fun mutableVisit(enumerationField: MutableEnumerationFieldSchema): Boolean {
+        return adaptee.visit(enumerationField)
+    }
+
+    override fun mutableVisit(entityField: MutableEntityFieldSchema): Boolean {
+        return adaptee.visit(entityField)
+    }
+
+    // Primitives
+
+    override fun mutableVisit(boolean: BooleanSchema): Boolean {
+        return adaptee.visit(boolean)
+    }
+
+    override fun <T : Number> mutableVisit(number: MutableNumericSchema<T>): Boolean {
+        return adaptee.visit(number)
+    }
+
+    override fun mutableVisit(string: MutableStringSchema): Boolean {
+        return adaptee.visit(string)
+    }
+
+    override fun mutableVisit(password1Way: MutablePassword1WaySchema): Boolean {
+        return adaptee.visit(password1Way)
+    }
+
+    override fun mutableVisit(password2Way: MutablePassword2WaySchema): Boolean {
+        return adaptee.visit(password2Way)
+    }
+
+    override fun mutableVisit(uuid: MutableUuidSchema): Boolean {
+        return adaptee.visit(uuid)
+    }
+
+    override fun mutableVisit(blob: MutableBlobSchema): Boolean {
+        return adaptee.visit(blob)
+    }
+
+    override fun mutableVisit(timestamp: MutableTimestampSchema): Boolean {
+        return adaptee.visit(timestamp)
+    }
+
+    override fun mutableVisit(ipv4Address: MutableIpv4AddressSchema): Boolean {
+        return adaptee.visit(ipv4Address)
+    }
+
+    override fun mutableVisit(ipv6Address: MutableIpv6AddressSchema): Boolean {
+        return adaptee.visit(ipv6Address)
+    }
+
+    override fun mutableVisit(macAddress: MutableMacAddressSchema): Boolean {
+        return adaptee.visit(macAddress)
+    }
+}
