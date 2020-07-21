20c23e2d9443e9f4d7f3e47ebfdba0fcd4f1c8ec
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2020-07-21 11:09:11 -0700

    Test following of a wildcard model

--- src/test/kotlin/org/tree_ware/model/cursor/FollowerModelCursorTests.kt
+++ src/test/kotlin/org/tree_ware/model/cursor/FollowerModelCursorTests.kt
@@ -1,9 +1,13 @@
 package org.tree_ware.model.cursor
 
+import org.tree_ware.common.codec.JsonWireFormatEncoder
+import org.tree_ware.model.codec.ModelEncodingVisitor
 import org.tree_ware.model.core.ElementModel
 import org.tree_ware.model.getModel
 import org.tree_ware.schema.core.NamedElementSchema
 import org.tree_ware.schema.core.SchemaTraversalAction
+import java.io.File
+import java.io.StringWriter
 import kotlin.test.*
 
 class ModelFollowerCursorTests {
@@ -15,6 +19,14 @@ class ModelFollowerCursorTests {
     @Test
     fun `Follower-cursor on different data-model follows leader-cursor`() {
         testFollowerDifferentModelInstances("src/test/resources/model/address_book_1.json")
+    }
+
+    @Test
+    fun `Follower-cursor on wildcard model follows leader-cursor`() {
+        testFollowerWildcardModelInstance(
+            "src/test/resources/model/address_book_1.json",
+            "src/test/resources/model/address_book_filter_all_model.json"
+        )
     }
 }
 
@@ -55,6 +67,40 @@ private fun testFollowerDifferentModelInstances(inputFilePath: String) {
         assertEquals(getPath(leaderMove.element), getPath(followerMove.element))
         assertEquals(getPath(leaderCursor.element), getPath(followerCursor.element))
     }
+}
+
+/**
+ * Verifies that the wildcard follower allows every leader element to be visited.
+ * To verify this, JSON is generated from the leader cursor and compared against
+ * the original JSON file (leaderFilePath).
+ */
+private fun testFollowerWildcardModelInstance(leaderFilePath: String, wildcardFilePath: String) {
+    val leaderModel = getModel<Unit>(leaderFilePath)
+    val followerModel = getModel<Unit>(wildcardFilePath)
+
+    val leaderCursor = LeaderModelCursor(leaderModel)
+    val followerCursor = FollowerModelCursor<Unit, Unit>(followerModel)
+
+    val jsonWriter = StringWriter()
+    val wireFormatEncoder = JsonWireFormatEncoder(jsonWriter, true)
+    val encodingVisitor = ModelEncodingVisitor<Unit>(wireFormatEncoder, null)
+
+    var action = SchemaTraversalAction.CONTINUE
+    while (action != SchemaTraversalAction.ABORT_TREE) {
+        val leaderMove = leaderCursor.next(action) ?: break
+        val followerMove = followerCursor.follow(leaderMove)
+        assertNotNull(followerMove)
+
+        when (leaderMove.direction) {
+            CursorMoveDirection.Visit -> action = leaderMove.element.visitSelf(encodingVisitor)
+            CursorMoveDirection.Leave -> leaderMove.element.leaveSelf(encodingVisitor)
+        }
+    }
+
+    val leaderFile = File(leaderFilePath)
+    val expected = leaderFile.readText()
+    val actual = jsonWriter.toString()
+    assertEquals(expected, actual)
 }
 
 // TODO(deepak-nulu): return model path instead of schema path
