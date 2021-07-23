6f34c781883aac6c01a47e827f109cb6b89b2c35
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2021-07-23 16:29:28 -0700

    Upgrade Gradle, Kotlin, and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "org.tree-ware"
 version = "1.0-SNAPSHOT"
 
-val kotlinVersion = "1.3.72"
+val kotlinCoroutinesVersion = "1.5.0"
 
 val jsonVersion = "1.1.4"
 
-val log4j2Version = "2.12.1"
+val log4j2Version = "2.14.1"
 
-val junitVersion = "5.4.2"
-val kotlinCoroutinesVersion = "1.2.2"
-val mockkVersion = "1.10.0"
+val mockkVersion = "1.12.0"
 
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.72")
+    id("org.jetbrains.kotlin.jvm").version("1.5.21")
     id("idea")
     id("java-library")
     id("java-test-fixtures")
@@ -30,8 +28,8 @@ tasks.withType<KotlinCompile> {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
 
     implementation("javax.json:javax.json-api:$jsonVersion")
     implementation("org.glassfish:javax.json:$jsonVersion")
@@ -39,16 +37,15 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:$log4j2Version")
     implementation("org.apache.logging.log4j:log4j-core:$log4j2Version")
 
-    testFixturesImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlinVersion")
+    testFixturesImplementation(kotlin("test"))
+    testImplementation(kotlin("test"))
 
     testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4j2Version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlinVersion")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$kotlinCoroutinesVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
     testImplementation("io.mockk:mockk:$mockkVersion")
 }
 
-tasks.withType<Test> {
+tasks.test {
     useJUnitPlatform()
 }
