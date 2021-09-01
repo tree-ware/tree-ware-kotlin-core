ce6162d9e78a488d433a847abb3a35ceee3876aa
Deepak Nulu <deepak@celona.io>
2021-08-31 21:30:42 -0700

    Generate UML diagram from meta-model

---
+++ src/test/kotlin/org/treeWare/model/metaModel/MetaModelDocs.kt
@@ -0,0 +1,25 @@
+package org.treeWare.model.metaModel
+
+import org.treeWare.metaModel.codec.encodeDot
+import org.treeWare.metaModel.getMetaName
+import org.treeWare.metaModel.getRootMeta
+import org.treeWare.metaModel.newAddressBookMetaModel
+import java.io.File
+import kotlin.test.Test
+
+// TODO(deepak-nulu): make doc generation a gradle task
+
+class MetaModelDocs {
+    @Test
+    fun `Generate address-book meta-model docs`() {
+        val metaModel = newAddressBookMetaModel()
+
+        val metaModelName = getMetaName(getRootMeta(metaModel))
+        val fileName = "${metaModelName}_meta_model"
+        val fileWriter = File("${fileName}.dot").bufferedWriter()
+        encodeDot(metaModel, fileWriter)
+        fileWriter.flush()
+
+        Runtime.getRuntime().exec("dot -Tpng ${fileName}.dot -o ${fileName}.png").waitFor()
+    }
+}
