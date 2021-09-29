469c5e087b7e81b67527fdb86cb542e3c33ea699
Deepak Nulu <deepak@celona.io>
2021-09-29 11:26:30 -0700

    Validate 'set' fields

--- src/test/kotlin/org/treeWare/model/metaModel/validation/KeyValidationTests.kt
+++ src/test/kotlin/org/treeWare/model/metaModel/validation/KeyValidationTests.kt
@@ -16,9 +16,16 @@ class KeyValidationTests {
 
     @Test
     fun `Multiplicity must not be 'list' for key fields`() {
-        val metaModelJson = getMetaModelJson("list")
+        val metaModelJson = getNonCompositionListMetaModelJson()
         val expectedErrors =
-            0.until(FIELD_COUNT).map { "Package 1 entity 0 field $it is a key but not defined as required" }
+            0.until(FIELD_COUNT - 1).map { "Package 1 entity 0 field $it is a key but not defined as required" }
+        assertJsonStringValidationErrors(metaModelJson, expectedErrors)
+    }
+
+    @Test
+    fun `Multiplicity must not be 'set' for key fields`() {
+        val metaModelJson = getCompositionSetMetaModelJson()
+        val expectedErrors = listOf("Package 1 entity 0 field 0 is a key but not defined as required")
         assertJsonStringValidationErrors(metaModelJson, expectedErrors)
     }
 
@@ -37,10 +44,10 @@ class KeyValidationTests {
     }
 }
 
+// NOTE: association fields cannot be keys, so they are not included below.
+// AssociationValidationTests ensures that associations cannot be keys.
 private fun getMetaModelJson(multiplicity: String?): String {
     val multiplicityJson = getMultiplicityJson(multiplicity)
-    // NOTE: association fields cannot be keys, so they are not included below.
-    // AssociationValidationTests ensures that associations cannot be keys.
     val mainPackageJson = """
         | {
         |   "name": "test.main",
@@ -64,6 +71,68 @@ private fun getMetaModelJson(multiplicity: String?): String {
         |           "is_key": true
         |           $multiplicityJson
         |         },
+        |         {
+        |           "name": "composition_field",
+        |           "type": "composition",
+        |           "composition": {
+        |             "name": "entity3",
+        |             "package": "test.common"
+        |           },
+        |           "is_key": true
+        |           $multiplicityJson
+        |         }
+        |       ]
+        |     }
+        |   ]
+        | }
+    """.trimMargin()
+    return newTestMetaModelJson(testMetaModelCommonRootJson, testMetaModelCommonPackageJson, mainPackageJson)
+}
+
+// NOTE: Compositions cannot be lists, hence getMetaModelJson() cannot be used in some tests.
+private fun getNonCompositionListMetaModelJson(): String {
+    val multiplicityJson = getMultiplicityJson("list")
+    val mainPackageJson = """
+        | {
+        |   "name": "test.main",
+        |   "entities": [
+        |     {
+        |       "name": "main_entity1",
+        |       "fields": [
+        |         {
+        |           "name": "primitive_field",
+        |           "type": "string",
+        |           "is_key": true
+        |           $multiplicityJson
+        |         },
+        |         {
+        |           "name": "enumeration_field",
+        |           "type": "enumeration",
+        |           "enumeration": {
+        |             "name": "enumeration1",
+        |             "package": "test.common"
+        |           },
+        |           "is_key": true
+        |           $multiplicityJson
+        |         }
+        |       ]
+        |     }
+        |   ]
+        | }
+    """.trimMargin()
+    return newTestMetaModelJson(testMetaModelCommonRootJson, testMetaModelCommonPackageJson, mainPackageJson)
+}
+
+// NOTE: Currently only compositions can be sets, hence getMetaModelJson() cannot be used in some tests.
+private fun getCompositionSetMetaModelJson(): String {
+    val multiplicityJson = getMultiplicityJson("set")
+    val mainPackageJson = """
+        | {
+        |   "name": "test.main",
+        |   "entities": [
+        |     {
+        |       "name": "main_entity1",
+        |       "fields": [
         |         {
         |           "name": "composition_field",
         |           "type": "composition",
