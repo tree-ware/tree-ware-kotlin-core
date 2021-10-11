eafc951cc5464b62aa7e8ef6ce361068813f800e
Deepak Nulu <deepak@celona.io>
2021-10-11 15:03:01 -0700

    Support multi-file meta-model definitions
    Resolves #35

--- src/test/kotlin/org/treeWare/model/core/AddressBookMetaModelTests.kt
+++ src/test/kotlin/org/treeWare/model/core/AddressBookMetaModelTests.kt
@@ -1,15 +1,20 @@
 package org.treeWare.model.core
 
-import org.treeWare.metaModel.ADDRESS_BOOK_META_MODEL_FILE_PATH
-import org.treeWare.metaModel.newAddressBookMetaModel
+import org.treeWare.metaModel.ADDRESS_BOOK_META_MODEL_FILES
+import org.treeWare.metaModel.newMainMetaMetaModel
 import org.treeWare.model.assertMatchesJson
 import org.treeWare.model.encoder.EncodePasswords
+import org.treeWare.model.getMainModelFromJsonFile
 import kotlin.test.Test
 
 class AddressBookMetaModelTests {
     @Test
     fun `Address-book meta-model JSON codec round trip must be lossless`() {
-        val metaModel = newAddressBookMetaModel(null, null)
-        assertMatchesJson(metaModel, null, ADDRESS_BOOK_META_MODEL_FILE_PATH, EncodePasswords.NONE)
+        val metaMetaModel = newMainMetaMetaModel()
+        ADDRESS_BOOK_META_MODEL_FILES.forEach { file ->
+            println("Testing file: $file...")
+            val metaModel = getMainModelFromJsonFile<Resolved>(metaMetaModel, file)
+            assertMatchesJson(metaModel, null, file, EncodePasswords.NONE)
+        }
     }
 }
