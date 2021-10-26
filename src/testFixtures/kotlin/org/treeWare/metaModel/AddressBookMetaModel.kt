0f597a116937dcedf02414b7f959a38519063fd3
Deepak Nulu <deepak@celona.io>
2021-10-26 16:05:08 -0700

    Create a meta-model from a list of JSON files

--- src/testFixtures/kotlin/org/treeWare/metaModel/AddressBookMetaModel.kt
+++ src/testFixtures/kotlin/org/treeWare/metaModel/AddressBookMetaModel.kt
@@ -1,11 +1,8 @@
 package org.treeWare.metaModel
 
-import org.treeWare.metaModel.validation.validate
 import org.treeWare.model.core.Cipher
 import org.treeWare.model.core.Hasher
 import org.treeWare.model.core.MutableMainModel
-import org.treeWare.model.getMainModelFromJsonFile
-import org.treeWare.model.operator.union
 
 val ADDRESS_BOOK_META_MODEL_FILES = listOf(
     "metaModel/address_book_root.json",
@@ -13,11 +10,5 @@ val ADDRESS_BOOK_META_MODEL_FILES = listOf(
     "metaModel/address_book_city.json",
 )
 
-fun newAddressBookMetaModel(hasher: Hasher?, cipher: Cipher?): MutableMainModel {
-    val metaMetaModel = newMainMetaMetaModel()
-    val metaModelParts = ADDRESS_BOOK_META_MODEL_FILES.map { getMainModelFromJsonFile(metaMetaModel, it) }
-    val metaModel = union(metaModelParts)
-    val errors = validate(metaModel, hasher, cipher)
-    if (errors.isNotEmpty()) throw IllegalStateException("Address-book meta-model is not valid")
-    return metaModel
-}
+fun newAddressBookMetaModel(hasher: Hasher?, cipher: Cipher?): MutableMainModel =
+    newMetaModelFromFiles(ADDRESS_BOOK_META_MODEL_FILES, hasher, cipher)
