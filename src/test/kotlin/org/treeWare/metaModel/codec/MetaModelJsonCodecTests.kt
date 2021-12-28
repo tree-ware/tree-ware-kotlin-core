37a24d2d0129b455f71aab7ca6b46c1f7940c22a
Deepak Nulu <deepak@celona.io>
2021-12-28 10:47:08 -0800

    Remove duplicate test

--- src/test/kotlin/org/treeWare/metaModel/codec/MetaModelJsonCodecTests.kt
+++ src/test/kotlin/org/treeWare/metaModel/codec/MetaModelJsonCodecTests.kt
@@ -5,11 +5,10 @@ import org.treeWare.metaModel.newMainMetaMetaModel
 import org.treeWare.model.testRoundTrip
 import kotlin.test.Test
 
-private val metaMetaModel = newMainMetaMetaModel()
-
 class MetaModelJsonCodecTests {
     @Test
     fun `Meta-model JSON codec round trip must be lossless`() {
+        val metaMetaModel = newMainMetaMetaModel()
         ADDRESS_BOOK_META_MODEL_FILES.forEach { testRoundTrip(it, metaModel = metaMetaModel) }
     }
 }
