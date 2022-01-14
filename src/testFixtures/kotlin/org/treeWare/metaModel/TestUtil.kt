cf5026e55c90734498a612c4e29a77a143cecd7d
Deepak Nulu <deepak@celona.io>
2022-01-14 14:05:35 -0800

    Support aux plugins in meta-model test utilities

--- src/testFixtures/kotlin/org/treeWare/metaModel/TestUtil.kt
+++ src/testFixtures/kotlin/org/treeWare/metaModel/TestUtil.kt
@@ -1,7 +1,10 @@
 package org.treeWare.metaModel
 
+import org.treeWare.metaModel.aux.MetaModelAuxPlugin
 import org.treeWare.metaModel.validation.validate
+import org.treeWare.model.decoder.ModelDecoderOptions
 import org.treeWare.model.decoder.decodeJson
+import org.treeWare.model.decoder.stateMachine.MultiAuxDecodingStateMachineFactory
 import org.treeWare.util.getFileReader
 import java.io.Reader
 import java.io.StringReader
@@ -12,24 +15,45 @@ private val metaMetaModel = newMainMetaMetaModel()
 fun assertJsonStringValidationErrors(
     metaModelJsonString: String,
     expectedValidationErrors: List<String>,
-    expectedDecodeErrors: List<String> = listOf()
-) = assertJsonValidationErrors(StringReader(metaModelJsonString), expectedValidationErrors, expectedDecodeErrors)
-
+    expectedDecodeErrors: List<String> = listOf(),
+    options: ModelDecoderOptions = ModelDecoderOptions(),
+    vararg auxPlugins: MetaModelAuxPlugin
+) = assertJsonValidationErrors(
+    StringReader(metaModelJsonString),
+    expectedValidationErrors,
+    expectedDecodeErrors,
+    options,
+    *auxPlugins
+)
 
 fun assertJsonFileValidationErrors(
     metaModelJsonFile: String,
     expectedValidationErrors: List<String>,
-    expectedDecodeErrors: List<String> = listOf()
-) = assertJsonValidationErrors(getFileReader(metaModelJsonFile), expectedValidationErrors, expectedDecodeErrors)
+    expectedDecodeErrors: List<String> = listOf(),
+    options: ModelDecoderOptions = ModelDecoderOptions(),
+    vararg auxPlugins: MetaModelAuxPlugin
+) = assertJsonValidationErrors(
+    getFileReader(metaModelJsonFile),
+    expectedValidationErrors,
+    expectedDecodeErrors,
+    options,
+    *auxPlugins
+)
 
 private fun assertJsonValidationErrors(
     jsonReader: Reader,
     expectedValidationErrors: List<String>,
-    expectedDecodeErrors: List<String>
+    expectedDecodeErrors: List<String>,
+    options: ModelDecoderOptions,
+    vararg auxPlugins: MetaModelAuxPlugin
 ) {
-    val (metaModel, decodeErrors) = decodeJson(jsonReader, metaMetaModel)
-    val errors = if (metaModel != null) validate(metaModel, null, null)
-    else listOf("Meta-model decoding failed")
+    val multiAuxDecodingStateMachineFactory =
+        MultiAuxDecodingStateMachineFactory(*auxPlugins.map { it.auxName to it.auxDecodingStateMachineFactory }
+            .toTypedArray())
+    val (metaModel, decodeErrors) = decodeJson(jsonReader, metaMetaModel, options, multiAuxDecodingStateMachineFactory)
+    val errors = metaModel?.let {
+        validate(metaModel, null, null) + auxPlugins.flatMap { it.validate(metaModel) }
+    } ?: listOf("Meta-model decoding failed")
     assertEquals(expectedDecodeErrors.joinToString("\n"), decodeErrors.joinToString("\n"))
     assertEquals(expectedValidationErrors.joinToString("\n"), errors.joinToString("\n"))
 }
