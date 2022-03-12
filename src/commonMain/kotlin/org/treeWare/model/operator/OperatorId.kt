874c48bedcfd6bdd20a0a170409b6cbfb09ccf19
Deepak Nulu <deepak@celona.io>
2022-03-12 15:42:51 -0800

    Support custom handling of entities via operator delegates
    Resolves #115

---
+++ src/commonMain/kotlin/org/treeWare/model/operator/OperatorId.kt
@@ -0,0 +1,8 @@
+package org.treeWare.model.operator
+
+/**
+ * Marker interface for operator IDs.
+ *
+ * `Delegate` is the type of the operator's delegate interface.
+ */
+interface OperatorId<Delegate>
---
+++ src/commonTest/kotlin/org/treeWare/model/operator/OperatorDelegateRegistryTests.kt
@@ -0,0 +1,108 @@
+package org.treeWare.model.operator
+
+import kotlin.test.*
+
+private interface TestOperator1Delegate {
+    fun getName(): String
+}
+
+private class DefaultTestOperator1Delegate(private val name: String) : TestOperator1Delegate {
+    override fun getName(): String = name
+}
+
+private object TestOperator1Id : OperatorId<TestOperator1Delegate>
+
+private interface TestOperator2Delegate {
+    fun getName(): String
+}
+
+private class DefaultTestOperator2Delegate(private val name: String) : TestOperator2Delegate {
+    override fun getName(): String = name
+}
+
+private object TestOperator2Id : OperatorId<TestOperator2Delegate>
+
+private const val ENTITY_1_FULL_NAME = "/package_name/entity_1_name"
+private const val ENTITY_2_FULL_NAME = "/package_name/entity_2_name"
+
+class OperatorDelegateRegistryTests {
+    private val operatorDelegateRegistry = OperatorDelegateRegistry()
+
+    @Test
+    fun `OperatorDelegateRegistry must return the correct registry for an operator`() {
+        val operator1Entity1DelegateName = "operator1Entity1DelegateName"
+        val operator1Entity2DelegateName = "operator1Entity2DelegateName"
+        val operator2Entity1DelegateName = "operator2Entity1DelegateName"
+
+        operatorDelegateRegistry.add(
+            ENTITY_1_FULL_NAME,
+            TestOperator1Id,
+            DefaultTestOperator1Delegate(operator1Entity1DelegateName)
+        )
+        operatorDelegateRegistry.add(
+            ENTITY_2_FULL_NAME,
+            TestOperator1Id,
+            DefaultTestOperator1Delegate(operator1Entity2DelegateName)
+        )
+        operatorDelegateRegistry.add(
+            ENTITY_1_FULL_NAME,
+            TestOperator2Id,
+            DefaultTestOperator2Delegate(operator2Entity1DelegateName)
+        )
+
+        val delegateRegistry1: DelegateRegistry<TestOperator1Delegate>? = operatorDelegateRegistry.get(TestOperator1Id)
+        assertNotNull(delegateRegistry1)
+        assertEquals(operator1Entity1DelegateName, delegateRegistry1?.get(ENTITY_1_FULL_NAME)?.getName())
+        assertEquals(operator1Entity2DelegateName, delegateRegistry1?.get(ENTITY_2_FULL_NAME)?.getName())
+
+        val delegateRegistry2: DelegateRegistry<TestOperator2Delegate>? = operatorDelegateRegistry.get(TestOperator2Id)
+        assertNotNull(delegateRegistry2)
+        assertEquals(operator2Entity1DelegateName, delegateRegistry2?.get(ENTITY_1_FULL_NAME)?.getName())
+        assertNull(delegateRegistry2?.get(ENTITY_2_FULL_NAME)?.getName())
+    }
+
+    @Test
+    fun `OperatorDelegateRegistry must return null if delegates for an operator were not registered`() {
+        val operator1DelegateName = "operator1delegate"
+        val operator2DelegateName = "operator2delegate"
+
+        val delegateRegistry1: DelegateRegistry<TestOperator1Delegate>? = operatorDelegateRegistry.get(TestOperator1Id)
+        assertNull(delegateRegistry1)
+
+        val delegateRegistry2: DelegateRegistry<TestOperator2Delegate>? = operatorDelegateRegistry.get(TestOperator2Id)
+        assertNull(delegateRegistry2)
+    }
+
+    @Test
+    fun `OperatorDelegateRegistry must replace old delegate if a new delegate is registered`() {
+        val operator1Entity1Delegate1Name = "operator1Entity1Delegate1Name"
+        val operator1Entity1Delegate2Name = "operator1Entity1Delegate2Name"
+        val operator2Entity1DelegateName = "operator2Entity1DelegateName"
+
+        operatorDelegateRegistry.add(
+            ENTITY_1_FULL_NAME,
+            TestOperator1Id,
+            DefaultTestOperator1Delegate(operator1Entity1Delegate1Name)
+        )
+        operatorDelegateRegistry.add(
+            ENTITY_1_FULL_NAME,
+            TestOperator1Id,
+            DefaultTestOperator1Delegate(operator1Entity1Delegate2Name)
+        )
+        operatorDelegateRegistry.add(
+            ENTITY_1_FULL_NAME,
+            TestOperator2Id,
+            DefaultTestOperator2Delegate(operator2Entity1DelegateName)
+        )
+
+        val delegateRegistry1: DelegateRegistry<TestOperator1Delegate>? = operatorDelegateRegistry.get(TestOperator1Id)
+        assertNotNull(delegateRegistry1)
+        assertEquals(operator1Entity1Delegate2Name, delegateRegistry1?.get(ENTITY_1_FULL_NAME)?.getName())
+        assertNull(delegateRegistry1?.get(ENTITY_2_FULL_NAME)?.getName())
+
+        val delegateRegistry2: DelegateRegistry<TestOperator2Delegate>? = operatorDelegateRegistry.get(TestOperator2Id)
+        assertNotNull(delegateRegistry2)
+        assertEquals(operator2Entity1DelegateName, delegateRegistry2?.get(ENTITY_1_FULL_NAME)?.getName())
+        assertNull(delegateRegistry2?.get(ENTITY_2_FULL_NAME)?.getName())
+    }
+}
