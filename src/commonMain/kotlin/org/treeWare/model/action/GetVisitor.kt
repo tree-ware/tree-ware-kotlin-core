3413345336e51c7dbf3d93262feee5b38533700d
Deepak Nulu <deepak@celona.io>
2022-05-06 15:39:38 -0700

    Remove old "get" action code, resources, and tests
    The new get functionality will be added as an operator. The concept of
    actions ends here.

--- src/commonMain/kotlin/org/treeWare/model/action/GetVisitor.kt
+++
@@ -1,6 +0,0 @@
-package org.treeWare.model.action
-
-import org.treeWare.model.traversal.Leader1Follower2ModelVisitor
-import org.treeWare.model.traversal.TraversalAction
-
-interface GetVisitor : Leader1Follower2ModelVisitor<TraversalAction>
