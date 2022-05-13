a20f7a8b51bc3bd0e89b0ed278e967133f52d002
Deepak Nulu <deepak@celona.io>
2022-05-13 11:59:29 -0700

    Decode JSON into model fields
    Resolves #130

--- src/jvmMain/kotlin/org/treeWare/model/decoder/JsonWireFormatDecoder.kt
+++ src/jvmMain/kotlin/org/treeWare/model/decoder/JsonWireFormatDecoder.kt
@@ -1,6 +1,6 @@
 package org.treeWare.model.decoder
 
-import org.treeWare.model.decoder.stateMachine.DecodingStateMachine
+import org.treeWare.model.decoder.stateMachine.DelegatingStateMachine
 import java.io.Reader
 import javax.json.Json
 import javax.json.stream.JsonParser
@@ -10,22 +10,22 @@ class JsonWireFormatDecoder : WireFormatDecoder {
     /**
      * @return an error message if there is an error.
      */
-    override fun decode(reader: Reader, decodingStateMachine: DecodingStateMachine): String? {
+    override fun decode(reader: Reader, delegatingStateMachine: DelegatingStateMachine): String? {
         val parser: JsonParser = Json.createParser(reader)
         var success = true
         while (parser.hasNext() && success) {
             val event = parser.next()
             success = when (event) {
-                JsonParser.Event.START_ARRAY -> decodingStateMachine.decodeListStart()
-                JsonParser.Event.START_OBJECT -> decodingStateMachine.decodeObjectStart()
-                JsonParser.Event.KEY_NAME -> decodingStateMachine.decodeKey(parser.string)
-                JsonParser.Event.VALUE_STRING -> decodingStateMachine.decodeStringValue(parser.string)
-                JsonParser.Event.VALUE_NUMBER -> decodingStateMachine.decodeNumericValue(parser.string)
-                JsonParser.Event.VALUE_TRUE -> decodingStateMachine.decodeBooleanValue(true)
-                JsonParser.Event.VALUE_FALSE -> decodingStateMachine.decodeBooleanValue(false)
-                JsonParser.Event.VALUE_NULL -> decodingStateMachine.decodeNullValue()
-                JsonParser.Event.END_OBJECT -> decodingStateMachine.decodeObjectEnd()
-                JsonParser.Event.END_ARRAY -> decodingStateMachine.decodeListEnd()
+                JsonParser.Event.START_ARRAY -> delegatingStateMachine.decodeListStart()
+                JsonParser.Event.START_OBJECT -> delegatingStateMachine.decodeObjectStart()
+                JsonParser.Event.KEY_NAME -> delegatingStateMachine.decodeKey(parser.string)
+                JsonParser.Event.VALUE_STRING -> delegatingStateMachine.decodeStringValue(parser.string)
+                JsonParser.Event.VALUE_NUMBER -> delegatingStateMachine.decodeNumericValue(parser.string)
+                JsonParser.Event.VALUE_TRUE -> delegatingStateMachine.decodeBooleanValue(true)
+                JsonParser.Event.VALUE_FALSE -> delegatingStateMachine.decodeBooleanValue(false)
+                JsonParser.Event.VALUE_NULL -> delegatingStateMachine.decodeNullValue()
+                JsonParser.Event.END_OBJECT -> delegatingStateMachine.decodeObjectEnd()
+                JsonParser.Event.END_ARRAY -> delegatingStateMachine.decodeListEnd()
             }
         }
         return if (success) null else "JSON decoding failed at: ${parser.location}"
--- src/jvmMain/kotlin/org/treeWare/model/decoder/WireFormatDecoder.kt
+++ src/jvmMain/kotlin/org/treeWare/model/decoder/WireFormatDecoder.kt
@@ -1,11 +1,11 @@
 package org.treeWare.model.decoder
 
-import org.treeWare.model.decoder.stateMachine.DecodingStateMachine
+import org.treeWare.model.decoder.stateMachine.DelegatingStateMachine
 import java.io.Reader
 
 interface WireFormatDecoder {
     /**
      * @return an error message if there is an error.
      */
-    fun decode(reader: Reader, decodingStateMachine: DecodingStateMachine): String?
+    fun decode(reader: Reader, delegatingStateMachine: DelegatingStateMachine): String?
 }
