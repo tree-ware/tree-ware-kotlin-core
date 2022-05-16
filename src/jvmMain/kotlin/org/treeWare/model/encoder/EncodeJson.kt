7511758aebb1896a6b88feafacc4fa92129b207a
Deepak Nulu <deepak@celona.io>
2022-05-16 15:29:56 -0700

    Add a function to JSON-encode a list of elements

--- src/jvmMain/kotlin/org/treeWare/model/encoder/EncodeJson.kt
+++ src/jvmMain/kotlin/org/treeWare/model/encoder/EncodeJson.kt
@@ -17,3 +17,23 @@ fun encodeJson(
     val encodingVisitor = ModelEncodingVisitor(wireFormatEncoder, multiAuxEncoder, encodePasswords)
     return forEach(element, encodingVisitor, true) != TraversalAction.ABORT_TREE
 }
+
+fun encodeJson(
+    elements: List<ElementModel>,
+    writer: Writer,
+    multiAuxEncoder: MultiAuxEncoder = MultiAuxEncoder(),
+    encodePasswords: EncodePasswords = EncodePasswords.NONE,
+    prettyPrint: Boolean = false,
+    indentSizeInSpaces: Int = 2
+): Boolean {
+    val wireFormatEncoder = JsonWireFormatEncoder(writer, prettyPrint, indentSizeInSpaces)
+    val encodingVisitor = ModelEncodingVisitor(wireFormatEncoder, multiAuxEncoder, encodePasswords)
+    wireFormatEncoder.encodeListStart(null)
+    elements.forEach { element ->
+        println("#### encodeJson() list element: $element")
+        val action = forEach(element, encodingVisitor, true)
+        if (action == TraversalAction.ABORT_TREE) return false
+    }
+    wireFormatEncoder.encodeListEnd()
+    return true
+}
