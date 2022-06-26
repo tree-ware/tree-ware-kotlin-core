e9fe533969bdfa8882956b0135e3dfd2cc50c47a
Deepak Nulu <deepak@celona.io>
2022-06-26 11:01:17 -0700

    Implement permitSet operator
    For #57

--- src/commonMain/kotlin/org/treeWare/model/operator/rbac/aux/PermissionsAux.kt
+++ src/commonMain/kotlin/org/treeWare/model/operator/rbac/aux/PermissionsAux.kt
@@ -14,6 +14,23 @@ data class PermissionsAux(
     val all: PermissionScope? = null,
 ) {
     fun getReadScope(): PermissionScope? = read ?: crud ?: all
+
+    fun isCreatePermitted(): Boolean = (create ?: crud ?: all)?.let { it != PermissionScope.NONE } ?: false
+    fun isUpdatePermitted(): Boolean = (update ?: crud ?: all)?.let { it != PermissionScope.NONE } ?: false
+    fun isDeletePermitted(): Boolean = (delete ?: crud ?: all)?.let { it != PermissionScope.NONE } ?: false
+
+    /** Return true if any write (CREATE, UPDATE, DELETE) access is permitted. */
+    fun isAnyWritePermitted(): Boolean = isCreatePermitted() || isUpdatePermitted() || isDeletePermitted()
+
+    /** Return sub-tree scoped permissions in a new instance. */
+    fun getSubTreePermissions(): PermissionsAux = PermissionsAux(
+        create = create.takeIf { it == PermissionScope.SUB_TREE },
+        read = read.takeIf { it == PermissionScope.SUB_TREE },
+        update = update.takeIf { it == PermissionScope.SUB_TREE },
+        delete = delete.takeIf { it == PermissionScope.SUB_TREE },
+        crud = crud.takeIf { it == PermissionScope.SUB_TREE },
+        all = all.takeIf { it == PermissionScope.SUB_TREE },
+    )
 }
 
 enum class PermissionScope {
