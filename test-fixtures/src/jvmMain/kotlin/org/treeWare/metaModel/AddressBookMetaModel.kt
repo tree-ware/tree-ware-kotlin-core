f163304966d1da6453e3d71d7954d2af075ae409
Deepak Nulu <deepak@celona.io>
2022-06-26 11:00:44 -0700

    Add a geo.point field to the test meta-model

--- test-fixtures/src/jvmMain/kotlin/org/treeWare/metaModel/AddressBookMetaModel.kt
+++ test-fixtures/src/jvmMain/kotlin/org/treeWare/metaModel/AddressBookMetaModel.kt
@@ -7,6 +7,7 @@ val ADDRESS_BOOK_META_MODEL_FILES = listOf(
     "metaModel/address_book_root.json",
     "metaModel/address_book_main.json",
     "metaModel/address_book_city.json",
+    "org/treeWare/metaModel/geo.json"
 )
 
 fun newAddressBookMetaModel(hasher: Hasher?, cipher: Cipher?): ValidatedMetaModel = newMetaModelFromJsonFiles(
