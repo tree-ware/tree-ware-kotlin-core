f1ccc06a0c1912c68c972697c1fa62f4ca113faa
Deepak Nulu <deepak@celona.io>
2022-06-30 17:47:06 -0700

    Extract validation of set-request from set()
    The set-pipeline needs to be: decode -> validate -> RBAC -> set().
    Validation needs to be moved out of set() so that it can be used
    earlier in the set-pipeline.

--- src/commonMain/kotlin/org/treeWare/model/operator/Set.kt
+++ src/commonMain/kotlin/org/treeWare/model/operator/Set.kt
@@ -4,7 +4,6 @@ import org.treeWare.model.core.EntityModel
 import org.treeWare.model.core.MainModel
 import org.treeWare.model.operator.set.SetDelegate
 import org.treeWare.model.operator.set.SetDelegateVisitor
-import org.treeWare.model.operator.set.SetRequestValidationVisitor
 import org.treeWare.model.traversal.forEach
 
 interface SetEntityDelegate {
@@ -21,12 +20,6 @@ fun set(
     setDelegate: SetDelegate,
     entityDelegates: EntityDelegateRegistry<SetEntityDelegate>?
 ): List<ElementModelError> {
-    // TODO(performance): traverse all visitors together.
-
-    val validationVisitor = SetRequestValidationVisitor()
-    forEach(main, validationVisitor, false)
-    if (validationVisitor.errors.isNotEmpty()) return validationVisitor.errors
-
     val beginErrors = setDelegate.begin()
     if (beginErrors.isNotEmpty()) return beginErrors
 
