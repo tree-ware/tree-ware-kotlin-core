0d00a06486a26c02e3d86a535c490060af9e51f2
Deepak Nulu <deepak@celona.io>
2022-09-09 13:35:06 -0700

    Populate granular sub-trees in get-requests

---
+++ src/commonMain/kotlin/org/treeWare/model/operator/PopulateSubTreeGranularityGetRequest.kt
@@ -0,0 +1,32 @@
+package org.treeWare.model.operator
+
+import org.treeWare.metaModel.Granularity
+import org.treeWare.metaModel.getGranularityMeta
+import org.treeWare.model.core.MutableEntityModel
+import org.treeWare.model.core.MutableMainModel
+import org.treeWare.model.traversal.AbstractLeader1MutableModelVisitor
+import org.treeWare.model.traversal.TraversalAction
+import org.treeWare.model.traversal.mutableForEach
+
+/**
+ * Populate the sub-trees of sub-tree granularity entities that are being fetched.
+ * A sub-tree is populated only if the get-request specifies just the root of the sub-tree.
+ * If the get-request specifies paths under the sub-tree root, then that sub-tree is not populated.
+ */
+fun populateSubTreeGranularityGetRequest(getRequest: MutableMainModel) {
+    val visitor = PopulateSubTreeGranularityGetRequestVisitor()
+    mutableForEach(getRequest, visitor, false)
+}
+
+private class PopulateSubTreeGranularityGetRequestVisitor :
+    AbstractLeader1MutableModelVisitor<TraversalAction>(TraversalAction.CONTINUE) {
+    override fun visitMutableEntity(leaderEntity1: MutableEntityModel): TraversalAction {
+        val parentField = leaderEntity1.parent
+        val parentFieldMeta = requireNotNull(parentField.meta)
+        val granularityMeta = getGranularityMeta(parentFieldMeta)
+        if (granularityMeta != Granularity.SUB_TREE) return TraversalAction.CONTINUE
+
+        populateSubTree(leaderEntity1, true)
+        return TraversalAction.ABORT_SUB_TREE
+    }
+}
