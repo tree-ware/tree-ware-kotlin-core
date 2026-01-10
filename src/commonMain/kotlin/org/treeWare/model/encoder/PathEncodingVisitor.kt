e69edb27c16043871ddaab06e201b97e33d116b4
Balaji <tamilbalaji006@gmail.com>
2026-01-10 21:56:39 +0530

    Implmenting pathencoding visitor

--- src/commonMain/kotlin/org/treeWare/model/encoder/PathEncodingVisitor.kt
+++ src/commonMain/kotlin/org/treeWare/model/encoder/PathEncodingVisitor.kt
@@ -1,7 +1,62 @@
 package org.treeWare.model.encoder
 
+import okio.BufferedSink
+import org.treeWare.model.core.*
 import org.treeWare.model.operator.ModelPathStack
+import org.treeWare.model.traversal.*
 
-class PathEncodingVisitor {
+class PathEncodingVisitor(
+    private val sink: BufferedSink
+) : ModelTraversalVisitor {
+
     private val pathStack = ModelPathStack()
+
+    override fun visitEntity(entity: EntityModel): TraversalAction {
+        pathStack.pushEntity(entity)
+        return TraversalAction.CONTINUE
+    }
+
+    override fun leaveEntity(entity: EntityModel) {
+        pathStack.pop()
+    }
+
+    override fun visitCompositionField(field: CompositionFieldModel): TraversalAction {
+        pathStack.pushField(field)
+        return TraversalAction.CONTINUE
+    }
+
+    override fun leaveCompositionField(field: CompositionFieldModel) {
+        pathStack.pop()
+    }
+
+    override fun visitPrimitiveField(field: PrimitiveFieldModel): TraversalAction {
+        pathStack.pushField(field)
+
+        sink.writeUtf8(pathStack.getCurrentPath())
+        sink.writeUtf8("\n")
+
+        return TraversalAction.CONTINUE
+    }
+
+    override fun leavePrimitiveField(field: PrimitiveFieldModel) {
+        pathStack.pop()
+    }
+
+    override fun visitListField(field: ListFieldModel): TraversalAction {
+        pathStack.pushField(field)
+        return TraversalAction.CONTINUE
+    }
+
+    override fun leaveListField(field: ListFieldModel) {
+        pathStack.pop()
+    }
+
+    override fun visitListItem(item: ListItemModel): TraversalAction {
+        pathStack.pushListItem(item)
+        return TraversalAction.CONTINUE
+    }
+
+    override fun leaveListItem(item: ListItemModel) {
+        pathStack.pop()
+    }
 }
